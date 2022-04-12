bf21033aa655b6b4f18b5be485071bb959f82b68
Lukas Tucek <sp7dy@seznam.cz>
2022-04-12 22:53:04 +0200

    first_commit

--- src/components/Komponenta/index.jsx
+++ src/components/Komponenta/index.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 
 const Komponenta = () => {
+
+	const [name, setName] = useState('________');
+	const [souhlas, setSouhlas] = useState(false);
+	const [mesto, setMesto] = useState('Brno');
+
+	const handleChange = (e) => {
+		setName(e.target.value); //ptám se na hodnotu prvku, který tu událost vyvolal
+	}
+
 	return (
 		<div className="komponenta">
-			<h3>Komponenta</h3>
-			<p>Ahoj, já jsem komponenta</p>
+			<h3>Napiš své jméno</h3>
+			<input type="text" onChange={handleChange} /> 
+
+			<p>Jméno je <strong>{name}</strong></p>
+
+			{name.length < 5 && <p>Jméno musí být nejméně 5 písmen</p>}
+			{name.length > 10 && <p>Máš moc dlouhé jméno</p>}
+
+			<hr />
+
+
+			<select value={mesto} onChange={ (e) => {setMesto(e.target.value)}}>
+				<option value="PHA">Praha</option> 
+				<option value="BRN">Brno</option>
+				<option value="OVA">Ostrava</option>
+			</select>
+
+			<p>Vybrané město je <strong>{mesto}</strong></p>
+
+			<label>
+				<input 
+					type="checkbox" 
+					checked={souhlas} 
+					onChange={ (e) => {setSouhlas(e.target.checked)}}  //podle toho, jestli je nebo neni zaškrtlé, nastavuju stav
+				/>
+				Souhlasím s podmínkami
+			</label>
+			<br />
+			<button disabled={!souhlas}>Objednat</button>
+
 		</div>
 	)
 }
--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import Komponenta from './components/Komponenta';
 const App = () => {
   return (
     <>
-      <h1>Práce v lekci 8</h1>
+      <h1>Práce v lekci 8 - formulář</h1>
       <Komponenta />
     </>
   );
